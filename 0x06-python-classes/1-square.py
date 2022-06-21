62df4c1fab47efb21b204d384c5d2ae2807ac12e
elisAlphaCode <elis.joshua55@gmail.com>
2022-06-21 17:29:27 +0100

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Calss initialization"""
+        self.__size = size
