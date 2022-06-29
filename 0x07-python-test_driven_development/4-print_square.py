152751562feaba7343e7f24dbf0a5e1babb0a684
elisAlphaCode <99659934+elisAlphaCode@users.noreply.github.com>
2022-06-29 10:09:53 +0100

    a function that prints a square with the character #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-
-"""This is the "4-print_square" module.
-This module supplies one function, print_square(size):
+"""
+This module defines `print_square`
+The function prints a square
 """
 
 
 def print_square(size):
+    """prints a square with size, `size`
+    Args:
+        size (int)
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
     """
-    prints a square with the character #
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if size > 0:
-        print(("#" * size + "\n") * size, end="")
+
+    if type(size) != int:
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise ValueError('size must be >= 0')
+
+    for x in range(size):
+        print('#' * size)
