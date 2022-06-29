9a0be0d2275e892de33d3df8bbb09cc89896ed83
elisAlphaCode <99659934+elisAlphaCode@users.noreply.github.com>
2022-06-29 09:53:19 +0100

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
-This module defines `print_square`
+This module defines `print_square
+
 The function prints a square
 """
 
 
 def print_square(size):
     """prints a square with size, `size`
+    
     Args:
         size (int)
+        
     Raises:
         TypeError: size must be an integer
         ValueError: size must be >= 0
