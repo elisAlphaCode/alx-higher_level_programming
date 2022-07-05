7e220c1e7ed24e12e1ee08735a116f4af5cc342b
elisAlphaCode <elis.joshua55@gmail.com>
2022-07-05 19:26:26 +0100

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-""" module that contains from_json_string function """
+"""
+    Python script that adds all args to a Python List.
+    List is then saved to a file.
+"""
+
+
+import sys
 import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file = "add_item.json"
+json_list = []
 
-def from_json_string(my_str):
-    """
-    function that returns an object (Python data structure)
-    represented by a JSON string:
-    """
-    data = json.loads(my_str)
-    return data
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
+"""
+    Python script that adds all args to a Python List.
+    List is then saved to a file.
+"""
 
-"""Module to load and save file"""
+
 import sys
+import json
+import os.path
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+file = "add_item.json"
+json_list = []
 
-def main():
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__(
-        '6-load_from_json_file').load_from_json_file
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
 
-
-main()
+save_to_json_file(json_list, file)
