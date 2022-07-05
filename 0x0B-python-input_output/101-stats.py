a779ced3a053d7655706cdc1fb73f505a20d6f6f
elisAlphaCode <elis.joshua55@gmail.com>
2022-07-05 20:03:14 +0100

    a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,9 +2,9 @@
 """
 Module for log parsing scripts.
 """
+
+
 import sys
-
-
 if __name__ == "__main__":
     size = [0]
     codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
