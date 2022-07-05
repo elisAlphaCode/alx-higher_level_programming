db188c2a0edbf725c0be168fd439a71d4d4a0f8e
elisAlphaCode <elis.joshua55@gmail.com>
2022-07-05 19:30:32 +0100

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -4,6 +4,7 @@
     List is then saved to a file.
 """
 
+
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
