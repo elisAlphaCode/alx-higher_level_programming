3e3b5bd698c8864a7b984e221983fe29c0d2c7f8
elisAlphaCode <elis.joshua55@gmail.com>
2022-08-10 18:38:44 +0100

    Creates the database hbtn_0d_usa with the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,10 +2,10 @@
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    PRIMARY KEY (`id`),
     `id` INT NOT NULL,
     `state_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `name` VARCHAR(256) NOT NULL,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`state_id`),
     REFERENCES hbtn_0d_usa.states (`id`)
 );
