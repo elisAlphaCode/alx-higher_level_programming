9cb535b8a905a55507da1723f8f0d5f310577f0a
elisAlphaCode <elis.joshua55@gmail.com>
2022-08-10 17:51:10 +0100

    Creates the Database hbtn_0d_usa with table states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- Creates the Database hbtn_0d_usa with table states.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.state (
-    PRIMARY KEY (`id`),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
     `id` INT UNIQUE   NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(256) NOT NULL
+    `name` VARCHAR(256) NOT NULL,
+     PRIMARY KEY (`id`)
 );
