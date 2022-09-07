be15977b20e2f55ecf3f61cae36da8ecb265ce71
elisAlphaCode <elis.joshua55@gmail.com>
2022-09-07 05:58:08 +0100

    Prints using the C character

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const baseSquare = require('./5-square');
+
+module.exports = class Square extends baseSquare {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    this.print(c);
+  }
+};
