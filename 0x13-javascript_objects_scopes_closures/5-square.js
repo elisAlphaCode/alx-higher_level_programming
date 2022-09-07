6f50b9a393ea80be051ff1621b212807c455c6d3
elisAlphaCode <elis.joshua55@gmail.com>
2022-09-07 05:47:45 +0100

    A subclass

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,7 +2,7 @@
 // Import the base class: Rectangle
 const Rectangle = require('./4-rectangle');
 
-// Square subclasses base class
+// subclasses base class
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
