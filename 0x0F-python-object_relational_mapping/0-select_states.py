90df14bacafc5642ffc5804ee5829a41ed032438
elisAlphaCode <elis.joshua55@gmail.com>
2022-09-13 08:27:07 +0100

    select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,13 +11,8 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    mySQL_u = sys.argv[1]
-    mySQL_p = sys.argv[2]
-    db_name = sys.argv[3]
-    
-
-    # By default, it will connect to localhost:3306
-    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+ 
+    db = MySQLdb.connect(user=sys.argv[1],passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
     cur= db.cursor()
 
     cur.execute("SELECT * FROM states ORDER By id")
