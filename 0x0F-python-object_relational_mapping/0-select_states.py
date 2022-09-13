66d2f6ef91941e47592e11a2364807430497abd4
elisAlphaCode <elis.joshua55@gmail.com>
2022-09-13 07:35:52 +0100

    select all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Script that listd all `states` from the database `hbtn_0e_usa`
+Aruguments:
+    mysql username (str)
+    mysql password (str)
+    database name (str)
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mySQL_u = sys.argv[1]
+    mySQL_p = sys.argv[2]
+    db_name = sys.argv[3]
+    
+
+    # By default, it will connect to localhost:3306
+    db = MySQLdb.connect(user=mySQL_u, passwd=mySQL_p, db=db_name)
+    cur= db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER By id")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
